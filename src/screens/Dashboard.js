662b26249ce26fc5d17d9f7b7a8b1cfa6c87f3ba
Vasanth Reddy Baddam <vasanth@vasanths-mbp.myfiosgateway.com>
2021-11-22 16:41:16 -0500

    changed profile page

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,86 +1,193 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import { Button } from 'react-native-paper'
+import React from 'react';
 
-import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 
-function HomeScreen({ navigation }) {
+import HomeScreen from './HomeScreen';
+// import NotificationScreen from './NotificationScreen';
+// import ExploreScreen from './ExploreScreen';
+import ProfileScreen from './ProfileScreen';
+// import MapTestScreen from './MapTestScreen';
+import EditProfileScreen from './EditProfileScreen';
+
+import {useTheme, Avatar} from 'react-native-paper';
+import { View } from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+// import CardListScreen from './CardListScreen';
+// import CardItemDetails from './CardItemDetails';
+
+const HomeStack = createStackNavigator();
+const NotificationStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
+
+const Tab = createMaterialBottomTabNavigator();
+
+const MainTabScreen = () => (
+  <Tab.Navigator
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+
+        if (route.name === 'Feed') {
+          iconName = 'dynamic-feed'
+        } else if (route.name === 'Notifications') {
+          iconName = 'notifications'
+        } else if (route.name === 'Profile') {
+          iconName = 'account-circle'
+        } else if (route.name === 'Add') {
+          iconName = 'add'
+        }
+
+        // You can return any component that you like here!
+        return <MaterialIcons name={iconName} size={20} color={color} />;
+      },
+    })}>
+    <Tab.Screen name="Feed" component={HomeStackScreen} />
+    <Tab.Screen name="Add" component={ProfileStackScreen} />
+    <Tab.Screen name="Notifications" component={ProfileStackScreen} />
+    <Tab.Screen name="Profile" component={ProfileStackScreen} />
+  </Tab.Navigator>
+);
+
+
+export default MainTabScreen;
+
+const HomeStackScreen = ({navigation}) => {
+  const {colors} = useTheme();
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button
-        title="Go to Settings"
-        onPress={() => navigation.navigate('Profile')}
+    <HomeStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.background,
+          shadowColor: colors.background, // iOS
+          elevation: 0, // Android
+        },
+        headerTintColor: colors.text,
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <HomeStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Connect',
+          headerLeft: () => (
+            <View style={{marginLeft: 10}}>
+              <Icon.Button
+                name="ios-menu"
+                size={25}
+                color={colors.text}
+                backgroundColor={colors.background}
+                onPress={() => navigation.openDrawer()}
+              />
+            </View>
+          ),
+          headerRight: () => (
+            <View style={{flexDirection: 'row', marginRight: 10}}>
+              <TouchableOpacity
+                style={{paddingHorizontal: 10, marginTop: 5}}
+                onPress={() => {
+                  navigation.navigate('Profile');
+                }}>
+                <Avatar.Image
+                  source={{
+                    uri:
+                      'https://api.adorable.io/avatars/80/abott@adorable.png',
+                  }}
+                  size={30}
+                />
+              </TouchableOpacity>
+            </View>
+          ),
+        }}
       />
-    </View>
+    </HomeStack.Navigator>
   );
-}
+};
 
-function NotificationsScreen({ navigation }) {
+const NotificationStackScreen = ({navigation}) => (
+  <NotificationStack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: '#1f65ff',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    }}>
+    <NotificationStack.Screen
+      name="Notifications"
+      component={NotificationScreen}
+      options={{
+        headerLeft: () => (
+          <Icon.Button
+            name="ios-menu"
+            size={25}
+            backgroundColor="#1f65ff"
+            onPress={() => navigation.openDrawer()}
+          />
+        ),
+      }}
+    />
+  </NotificationStack.Navigator>
+);
+
+const ProfileStackScreen = ({navigation}) => {
+  const {colors} = useTheme();
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Notifications!</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate('Profile')} />
-    </View>
+    <ProfileStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.background,
+          shadowColor: colors.background, // iOS
+          elevation: 0, // Android
+        },
+        headerTintColor: colors.text,
+      }}>
+      <ProfileStack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          title: '',
+          headerLeft: () => (
+            <View style={{marginLeft: 10}}>
+              <Icon.Button
+                name="ios-menu"
+                size={25}
+                backgroundColor={colors.background}
+                color={colors.text}
+                onPress={() => navigation.openDrawer()}
+              />
+            </View>
+          ),
+          headerRight: () => (
+            <View style={{marginRight: 10}}>
+              <MaterialCommunityIcons.Button
+                name="account-edit"
+                size={25}
+                backgroundColor={colors.background}
+                color={colors.text}
+                onPress={() => navigation.navigate('EditProfile')}
+              />
+            </View>
+          ),
+        }}
+      />
+      <ProfileStack.Screen
+        name="EditProfile"
+        options={{
+          title: 'Edit Profile',
+        }}
+        component={EditProfileScreen}
+      />
+    </ProfileStack.Navigator>
   );
-}
-
-function FeedScreen ({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Taskks!</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate('Profile')} />
-    </View>
-  );
-}
-
-function EventsScreen ({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Events!</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate('Profile')} />
-    </View>
-  );
-}
-
-
-const Tab = createBottomTabNavigator(); 
-
-export default function Dashboard() {
-  return (
-      <NavigationContainer independent = {true}>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-  
-              if (route.name === 'Feed') {
-                iconName = focused
-                  ? 'dynamic-feed'
-                  : 'dynamic-feed';
-              } else if (route.name === 'Notifications') {
-                iconName = 'notifications';
-              } else if (route.name === 'Post') {
-                iconName = 'add';
-              } else if (route.name === 'Profile') {
-                iconName = focused ? 'account-circle' : 'account-circle';
-              }
-
-  
-              // You can return any component that you like here!
-              return <MaterialIcons name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: 'tomato',
-            tabBarInactiveTintColor: 'gray',
-          })}
-        >
-          <Tab.Screen name="Feed" component={FeedScreen}/>
-          <Tab.Screen name="Post" component={EventsScreen} />
-          <Tab.Screen name="Notifications" component={NotificationsScreen} />
-          <Tab.Screen name="Profile" component={HomeScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-  )
-}
+};
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import {useTheme} from '@react-navigation/native';
+
+import Swiper from 'react-native-swiper';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import StarRating from '../components/StarRating';
+
+const HomeScreen = ({navigation}) => {
+  const theme = useTheme();
+
+  return (
+    <ScrollView style={styles.container}>
+      
+    </ScrollView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
